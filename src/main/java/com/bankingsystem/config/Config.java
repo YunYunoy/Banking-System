cb174b815a3cdbe48e01f93185ba030eb0b978f6
YunYunoy <seba12mik@interia.pl>
2023-07-27 22:14:07 +0200

    authorization for h2-database

--- src/main/java/com/bankingsystem/config/Config.java
+++ src/main/java/com/bankingsystem/config/Config.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -16,12 +17,23 @@ public class Config {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .authorizeHttpRequests(authorize -> {
+                    authorize
+                            .requestMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+                            .requestMatchers((new AntPathRequestMatcher("/h2-console/**"))).permitAll();
+                })
                 .authorizeHttpRequests()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .and()
-//                .formLogin()
-//                .and()
-                .csrf().disable();
+                .formLogin().and()
+                .httpBasic()
+                .and().csrf().disable();
+
+
+        http.headers().frameOptions().sameOrigin();
+
+
+        http.headers().frameOptions().sameOrigin();
 
         return http.build();
     }
